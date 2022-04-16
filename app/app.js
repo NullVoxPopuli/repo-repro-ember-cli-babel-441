d031dbb3d8b9f6d88dec1ef0fbf1d897856bc643
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-04-15 23:11:38 -0400

    Just run ember test

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'my-app-test-deprecate/config/environment';
+import { deprecate } from '@ember/debug';
+
+console.log(deprecate);
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
